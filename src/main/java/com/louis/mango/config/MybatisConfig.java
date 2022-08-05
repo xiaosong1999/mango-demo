de6120c6c8c85b5e1ecbbfe7b2fb10d6b550a389
Jerry <458208964@qq.com>
2022-08-05 15:52:52 +0800

    Initial commit

---
+++ src/main/java/com/louis/mango/config/MybatisConfig.java
@@ -0,0 +1,28 @@
+package com.louis.mango.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+@MapperScan("com.louis.mango.**.dao")
+public class MybatisConfig {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory=new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliasesPackage("com.louis.mango.**.model");
+        PathMatchingResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath*:**/sqlmap/*.xml"));
+        return sessionFactory.getObject();
+    }
+}
